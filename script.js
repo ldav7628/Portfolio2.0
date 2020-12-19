1e1152c60eef78073324877353bfc627fec32255
ldav7628 <54045982+ldav7628@users.noreply.github.com>
2020-12-19 18:08:31 +1100

    about me txt

--- script.js
+++ script.js
@@ -211,3 +211,18 @@ gsap.registerPlugin(ScrollTrigger);
     window.onbeforeunload = function() {
         window.scrollTo(0, 0);
     }
+	gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".hero__content-text",
+    scrub: 1,
+    start: "center bottom",
+    end: "+=80"
+  }
+});
+
+tl.from(".hero__content-text", {
+  y: 50,
+  opacity: 0
+});
